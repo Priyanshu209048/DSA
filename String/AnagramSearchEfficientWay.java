6da794f0bf556378d1f940e74def4892ae816cbf
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-05-26 21:10:42 +0530

    Add files via upload

---
+++ String/AnagramSearchEfficientWay.java
@@ -0,0 +1,38 @@
+package com.String;
+
+public class AnagramSearchEfficientWay {
+    static final int CHAR = 256;
+    static boolean areSame(int[] CT, int[] CP){
+        for (int i = 0; i < CHAR; i++) {
+            if (CT[i] != CP[i])
+                return false;
+        }
+        return true;
+    }
+
+    static boolean isPresent(String txt, String pat){
+        int[] CT = new int[CHAR];
+        int[] CP = new int[CHAR];
+        for (int i = 0; i < pat.length(); i++) {
+            CT[txt.charAt(i)]++;
+            CP[pat.charAt(i)]++;
+        }
+        for (int i = pat.length(); i < txt.length(); i++) {
+            if (areSame(CT, CP))
+                return true;
+            CT[txt.charAt(i)]++;
+            CT[txt.charAt(i - pat.length())]--;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String txt = "Priyanshu";
+        String pat = "asn";
+        if (isPresent(txt, pat))
+            System.out.println("Present");
+        else
+            System.out.println("Not Present");
+    }
+}
