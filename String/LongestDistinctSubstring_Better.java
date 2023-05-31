3f4344e996875c809d77c5d67f31d66be940a940
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-05-31 20:50:22 +0530

    Add files via upload

---
+++ String/LongestDistinctSubstring_Better.java
@@ -0,0 +1,27 @@
+package com.String;
+
+public class LongestDistinctSubstring_Better {
+
+    static int longestDistinct(String str){
+        int n = str.length();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[256];
+            for (int j = i; j < n; j++) {
+                if (visited[str.charAt(j)])
+                    break;
+                else {
+                    res = Math.max(res, j - i + 1);
+                    visited[str.charAt(j)] = true;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String str = "abac";
+        int len = longestDistinct(str);
+        System.out.println(len);
+    }
+}
